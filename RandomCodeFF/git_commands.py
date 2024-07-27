38115436c7dc1435ab2b293688d89f5af1917d78
Harish Paramathmananda <156591946+Harish141296@users.noreply.github.com>
2024-07-27 05:56:40 +0000

    FEAT: Git Branch merge and delete

---
+++ RandomCodeFF/git_commands.py
@@ -0,0 +1,11 @@
+"""
+git checkout -b new-branch
+# Make changes, then:
+git add .
+git commit -m "Your commit message"
+git checkout main
+git merge new-branch
+# Resolve conflicts if needed, then:
+git branch -d new-branch  # Optional
+
+"""
