2d30267fe761ec11878abc109b900ef0c9ba380c
Harish Paramathmananda <156591946+Harish141296@users.noreply.github.com>
2024-07-27 09:11:34 +0000

    FEAT: Mongodb select query using py

---
+++ mongodb_tutorial/fetching_sample_data.py
@@ -0,0 +1,28 @@
+# select query based on name 
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+ 
+# Access the variables
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
+
+# Connect to the MongoDB server
+client = MongoClient(f'mongodb+srv://{username}:{password}@jerry.yil7qvo.mongodb.net/')  # Replace with your MongoDB URI
+db = client['sample_airbnb']  # Replace with your database name
+collection = db['listingsAndReviews']  # Replace with your collection name
+
+
+# Perform the query
+document = collection.find_one({'name': 'Ribeira Charming Duplex'})
+
+# Print the result
+if document:
+    print(document)
+else:
+    print("No document found with the given _id")
