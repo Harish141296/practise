6923b0c9f0aa6a9ac8f4ded99879a620c5ceb636
Harish Paramathmananda <156591946+Harish141296@users.noreply.github.com>
2024-07-29 17:06:28 +0000

    FEAT: Templates created for main_app

--- django_tutorial_udemy/main/views.py
+++ django_tutorial_udemy/main/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def main_view(request):
-    return HttpResponse('<h1> Welcome back master.</h1>')
+    details = {'name': 'Jerry',
+               'profession': 'Senior Developer'}
+    return render(request, 'views/main.html', context = details)
