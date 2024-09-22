977d70340ba666c2f1383c0402ae8506a5b6a7a7
Harish141296 <harishparamathmananda@gmail.com>
2024-09-22 12:16:55 +0530

    FEAT: New Project using DRF

---
+++ drf/backend/api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+def api_home(request, *args, **kwargs):
+    body = request.body
+    print(body)
+    return JsonResponse({"message":"Hi there Master, Welcome back world needs you."})
---
+++ drf/py_client/basic.py
@@ -0,0 +1,11 @@
+import requests 
+
+endpoint = "http://127.0.0.1:8000/"
+
+get_response = requests.get(endpoint, params={'secret_key':'134sdf234df'}, json = {"name":"G.O.A.T"}) 
+
+print(get_response.json())
+# print(get_response.text)
+print(get_response.status_code)
+
+
